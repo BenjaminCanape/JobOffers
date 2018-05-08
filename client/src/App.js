419285fcc8470789c492fb7058fa15d5bb1bf7c2
Benjamin Canape <benjamin.canape@gmail.com>
2018-05-09 00:49:52 +0200

    print all job offers v1 ... just to test how to get API response in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      jobOfferList: []
+    }
+  }
+
+  componentDidMount() {
+    this.getJobOfferList()
+      .then(jobOfferList => this.setState({ jobOfferList: jobOfferList }))
+      .catch(err => console.log(err));
+  }
+
+  getJobOfferList = async () => {
+    const response = await fetch('/jobOffers');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  }
+
+  getShortDescription(description){
+    return description.substring(0,150);
+  }
+
   render() {
+    const {jobOfferList} = this.state
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+
+        {jobOfferList.length ? (
+          jobOfferList.map( ({title, company, city, jobDescription}, index) => (
+            <div key={index} className="jobOffer"> 
+              <a href="/" className="jobOfferTitle"> {title}  </a><br/>
+              <span>{company},{city}</span><br/>
+              <span>{this.getShortDescription(jobDescription) }</span>
+            </div>
+
+        ))) : 
+        ( <span> Aucune offre</span> )}
       </div>
     );
   }
